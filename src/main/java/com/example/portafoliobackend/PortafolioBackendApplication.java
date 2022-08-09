d0cb4b4e14d1e7ee8179ed41c3efb488fc644066
Luciano <orisluciano@gmail.com>
2022-08-08 21:07:47 -0300

    Primer commit

---
+++ src/main/java/com/example/portafoliobackend/PortafolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.portafoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/portafoliobackend/PortafolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portafoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
